ebf29c84cf01d50720942b88d8abb2935e8eba7a
Teyrro <2vvoropaevv2@gmail.com>
2022-02-13 22:37:16 +0700

    Add "Input size cashbox"

--- Лаба_1/Лаба_1/Program.cs
+++ Лаба_1/Лаба_1/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Laba_1;
+﻿using Laba_1;
 
 namespace lab1
 {
@@ -8,8 +7,18 @@ namespace lab1
         static void Main(string[] args)
         {
             HW1 test = new HW1();
+
+            Console.Write("Введите кол-во касс: ");
+            int cashbox = Convert.ToInt32(Console.ReadLine());
+
             int[] a = { 5, 3, 4 };
-            Console.WriteLine(test.QueueTime(a, 1));
+            Console.WriteLine("Очередь: {0}, {1}, {2}", a[0], a[1], a[2]);
+
+            int WasteTime = ((int)test.QueueTime(a, cashbox));
+
+            Console.WriteLine(
+                $"Время затраченное на всех покупателей:  {WasteTime}"
+                );
         }
     }
 }
